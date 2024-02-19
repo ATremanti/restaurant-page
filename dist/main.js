2e32faf83d1e704cea4de7d095fd7e6732264733
Arthur Tremanti <102254206+ATremanti@users.noreply.github>
2024-02-19 09:27:08 -0300

    fixed home page and created first append function

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,13 @@
-console.log("test");
+const content = document.querySelector('#content');
+
+function appendItems() {
+    const home = document.createElement('div')
+    home.classList.add('home-content')
+    content.appendChild(home)
+
+    const homep = document.createElement('p')
+    homep.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quidem aliquid provident atque maiores dolorem eos corrupti? Laborum placeat velit animi minus enim. Molestiae dicta ad molestias dolorum porro tenetur?"
+    home.appendChild(homep)
+}
+
+appendItems();
